9fa9380d71d70d624acc2e501ab0dc4e18b66503
13catko <13catko@gmail.com>
2024-05-03 11:33:26 +0500

    restoran

--- app/js/main.js
+++ app/js/main.js
@@ -1,27 +1,32 @@
 $(function () {
 
-  const swiperTwo = new Swiper('.popular-categoty__filter-list', {
-    loop: true,
-    slidesPerView: 5,
-    spaceBetween: 20,
+  // const swiperTwo = new Swiper('.popular-categoty__filter-list', {
+  //   loop: true,
+  //   slidesPerView: 5,
+  //   spaceBetween: 20,
 
-    pagination: {
-      el: '.swiper-pagination',
-    },
-    breakpoints: {
-      640: {
-        slidesPerView: 4,
-      },
-      480: {
-        slidesPerView: 3,
-      },
-      360: {
-        slidesPerView: 2,
-      },
-    },
+  //   pagination: {
+  //     el: '.swiper-pagination',
+  //   },
+  //   breakpoints: {
+  //     640: {
+  //       slidesPerView: 4,
+  //     },
+  //     480: {
+  //       slidesPerView: 3,
+  //     },
+  //     360: {
+  //       slidesPerView: 2,
+  //     },
+  //   },
 
-  });
-  
+  // });
+
+
+
+
+
+
 });
 
 var mixer = mixitup('.catalog');
