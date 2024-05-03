247409e430c363516e11e8e36acdd9f43bc5f334
13catko <13catko@gmail.com>
2024-05-03 16:20:12 +0500

    review start

--- app/js/main.js
+++ app/js/main.js
@@ -1,26 +1,13 @@
 $(function () {
 
-  // const swiperTwo = new Swiper('.popular-categoty__filter-list', {
-  //   loop: true,
-  //   slidesPerView: 5,
-  //   spaceBetween: 20,
 
-  //   pagination: {
-  //     el: '.swiper-pagination',
-  //   },
-  //   breakpoints: {
-  //     640: {
-  //       slidesPerView: 4,
-  //     },
-  //     480: {
-  //       slidesPerView: 3,
-  //     },
-  //     360: {
-  //       slidesPerView: 2,
-  //     },
-  //   },
-
-  // });
+  $('.reviews__inner, .test__inner').slick({
+    arrows: true,
+    dots: true,
+    fade: false,
+    autoplay: false,
+    autoplaySpeed: 2000,
+  });
 
 
 
