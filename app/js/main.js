30f4ec77ce4f2e85e494c39cf7e49f3a6760cb24
13catko <13catko@gmail.com>
2024-05-04 17:47:57 +0500

    finish 0.1

--- app/js/main.js
+++ app/js/main.js
@@ -2,12 +2,19 @@ $(function () {
 
 
   $('.reviews__inner, .test__inner').slick({
+
+    // prevArrow: '<button type="button" class="slider__arrow slider__arrow--prev"><span class="lnr lnr-chevron-left"></span></button>',
+    // nextArrow: '<button type="button" class="slider__arrow slider__arrow--next"><span class="lnr lnr-chevron-right"></span></button>',
+    // appendArrows: '.arrows-wrap',
+
     arrows: true,
     dots: true,
     fade: false,
     autoplay: false,
     autoplaySpeed: 2000,
   });
+
+  
 
 
 
