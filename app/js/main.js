16d20e3cd3683e92f80050e5fb43e3783752373b
13catko <13catko@gmail.com>
2024-05-17 13:01:03 +0500

    button

--- app/js/main.js
+++ app/js/main.js
@@ -3,9 +3,11 @@ $(function () {
 
   $('.reviews__inner, .test__inner').slick({
 
-    // prevArrow: '<button type="button" class="slider__arrow slider__arrow--prev"><span class="lnr lnr-chevron-left"></span></button>',
-    // nextArrow: '<button type="button" class="slider__arrow slider__arrow--next"><span class="lnr lnr-chevron-right"></span></button>',
-    // appendArrows: '.arrows-wrap',
+    prevArrow: '<button type="button" class="slider__arrow slider__arrow--prev"><span class="lnr lnr-chevron-left"></span></button>',
+
+    nextArrow: '<button type="button" class="slider__arrow slider__arrow--next"><span class="lnr lnr-chevron-right"></span></button>',
+    
+    appendArrows: '.arrows-wrap',
 
     arrows: true,
     dots: true,
