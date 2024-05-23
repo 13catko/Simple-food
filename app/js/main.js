3b0603c116f1aa298897d1cf57b01d0beec3c137
13catko <13catko@gmail.com>
2024-05-23 16:34:05 +0500

    burger

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,5 @@
 $(function () {
 
-
   $('.reviews__inner').slick({
 
     prevArrow: '<button type="button" class="slider__arrow slider__arrow--prev"><svg><use xlink: href="images/sprite.svg#icon-right"></use></svg></button>',
@@ -19,6 +18,36 @@ $(function () {
 
   });
 
+
+
+
+  const btn = document.querySelector('.menu__mobile-btn'); //наша кнопка
+  const btnclose = document.querySelector('.btn-close')
+  const mobileMenu = document.querySelector('.mobile-menu'); //мобильное меню
+  const bodyLock = document.querySelector('body'); //ищем как селектор ТЕГА
+
+
+
+  btn.addEventListener('click', () => {
+    mobileMenu.classList.add('menu--active'); //когда меню открыто
+    btnclose.classList.remove('menu--active')
+
+    if (mobileMenu.classList.contains('menu--active')) { //Проверяем, есть ли у меню активный класс
+      bodyLock.classList.add('lock'); //Блокируем скролл при открытом меню
+    }
+
+    else { //Когда нету активного класса у меню
+    
+      bodyLock.classList.remove('lock'); //Разрешаем скроллить
+    }
+  });
+
+  document.addEventListener('click', function (e) {
+    if (e.target !== btn && e.target !== mobileMenu) {
+      mobileMenu.classList.remove('menu--active');
+      bodyLock.classList.remove('lock');
+    }
+  });
 
 
 
