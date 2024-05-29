7f54603775831b992701e1ecedd8a212b3c2070c
13catko <13catko@gmail.com>
2024-05-29 17:06:28 +0500

    finish3.0

--- app/js/main.js
+++ app/js/main.js
@@ -13,8 +13,14 @@ $(function () {
     dots: true,
     fade: false,
     infinite: false,
-    autoplay: false,
+    autoplay: true,
     autoplaySpeed: 2000,
+    cssEase: 'linear',
+
+    breakpoint: 560,
+    settings: {
+      dots: false,
+    }
 
   });
 
@@ -25,19 +31,20 @@ $(function () {
   const btnclose = document.querySelector('.btn-close')
   const mobileMenu = document.querySelector('.mobile-menu'); //мобильное меню
   const bodyLock = document.querySelector('body'); //ищем как селектор ТЕГА
-
+  const fixed = document.querySelector('header')
 
 
   btn.addEventListener('click', () => {
-    mobileMenu.classList.add('menu--active'); //когда меню открыто
+    mobileMenu.classList.toggle('menu--active'); //когда меню открыто
     btnclose.classList.remove('menu--active')
 
     if (mobileMenu.classList.contains('menu--active')) { //Проверяем, есть ли у меню активный класс
       bodyLock.classList.add('lock'); //Блокируем скролл при открытом меню
+      // fixed.classList.remove('fixed-menu');
     }
 
     else { //Когда нету активного класса у меню
-    
+      
       bodyLock.classList.remove('lock'); //Разрешаем скроллить
     }
   });
@@ -51,13 +58,60 @@ $(function () {
 
 
 
+  let swiperTwo; // Объявляем переменную для свайпера
 
+  // Функция для инициализации свайпера
+  function initSwiper() {
+    swiperTwo = new Swiper('.restaurants__inner-list', {
+      loop: true,
+      slidesPerView: 1,
+      spaceBetween: 20,
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    });
+  }
+
+  // Функция для проверки ширины экрана и инициализации свайпера при необходимости
+  function checkScreenWidth() {
+    if (window.innerWidth <= 560) {
+      if (!swiperTwo) {
+        initSwiper();
+      }
+    } else {
+      if (swiperTwo) {
+        swiperTwo.destroy(); // Уничтожаем свайпер, если ширина экрана больше 560px
+        swiperTwo = undefined; // Обнуляем переменную свайпера
+      }
+    }
+  }
+
+  // Вызываем функцию проверки ширины экрана при загрузке страницы и при изменении размера окна
+  window.addEventListener('load', checkScreenWidth);
+  window.addEventListener('resize', checkScreenWidth);
+
+
+
+  window.addEventListener('scroll', function () {
+    let up_menu = document.getElementById('header');
+    let distance = window.scrollY; // Получаем текущее положение скролла
+
+    if (distance >= 159) {
+      up_menu.classList.add('fixed-menu');
+    } else {
+      up_menu.classList.remove('fixed-menu');
+    }
+  });
 
 
 
 });
 
 var mixer = mixitup('.catalog');
+
+
+
+
 
 // var mixer = mixitup('.catalog', {
   
@@ -70,4 +124,8 @@ var mixer = mixitup('.catalog');
 //   //   activeClass: 'on',
 //   // }
 // });
+// ////////////////////////////////////////////
+
+
+
 
